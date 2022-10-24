b654290c9bc8d47a524b735d6d90d2c1134ebb1d
Reevoc <61749102+Reevoc@users.noreply.github.com>
2022-10-24 19:20:30 +0200

    Project update
    Updated project with the function and the txt data file

--- 1-Project.R
+++ 1-Project.R
@@ -1,1 +1,31 @@
-s
+#TASK1: Take as input a numeric data matrix that is supposed to have the same format of the genetic data 
+#provided in stem
+#TASK2: Calculates the minor allele frequency q for each SNP
+#TASK3: Return the minor allele frequency of each SNP as a vector of numeric values with names corresponding 
+#to the SNP IDs (chromosome_position, e.g. “chr1_11169676”) with the same order they had in the 
+#input matrix
+#Suggestion: 
+#• It might be useful to use the function table() and to convert it in a data.frame(). 
+#• Sometime you might have 0 subject with genotype aa… c
+
+qcalculation <- function(SNPdata){
+  
+}
+#TASK1: Take as input a numeric data matrix that is supposed to have the same format of the genetic data 
+#provided in stem 
+#TASK2: Compute a HWE test for each SNP given as input 
+#• By calculating the chi^2 obs from the data
+#• By computing the p value using the function pchisq (DO NOT use directly the chisq.test() function)
+#TASK3: Return the HWE test p-values for each SNP as a vector of numeric values with names corresponding to 
+#the SNP IDs (chromosome_position, e.g. “chr1_11169676”) with the same order they had in the input 
+#matrix
+#Suggestion: be careful when you use pchisq(). The probability it gives as output by default is P[X ≤ chi^2
+                                                                                                 
+
+HWEtest <- (SNPdata){
+  
+}
+
+VARIANTanalysis <- function(filepath, indCTRL, MAFth = 0.01, HWEalpha = 0.01){
+  
+}
