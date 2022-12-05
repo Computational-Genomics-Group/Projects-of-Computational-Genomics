16e51774e9497f80c91f499bcbf136138d0fc889
Andriolo Matteo <matteo.andriolo.2@studenti.unipd.it>
2022-12-05 11:32:39 +0100

    refactored and fixed

--- Project2/ScriptProject2-partA.R
+++ Project2/ScriptProject2-partA.R
@@ -1,23 +1,22 @@
 MvAplot <- function(exprData,
                     pdffilename) {
-  namesamples = colnames(exprData)
-  nsamples = dim(exprData)[2]
+  samples.name = colnames(exprData)
+  samples.number = dim(exprData)[2]
   
   pdf(pdffilename)
   par(mfrow = c(2, 2))
   
-  for (i in 2:nsamples) {
-    M = log2(exprData[, 1] / exprData[, i])
-    A = 0.5 * log2(exprData[, 1] * exprData[, i])
+  for (i in 2:samples.number) {
+    M = log2(exprData[, 1]) - log2(exprData[, i])
+    A = 0.5 * (log2(exprData[, 1]) + log2(exprData[, i]))
     plot(
       A,
       M,
-      xlab = "A",
-      ylab = "M"
-      ,
-      main = sprintf("%s vs %s", namesamples[1], namesamples[i])
-      ,
-      pch = 20
+      xlab = "log-average | A",
+      ylab = "log-ratio | M",
+      main = sprintf("%s vs %s", samples.name[1], samples.name[i]),
+      pch = 20,
+      type = "p"
     )
     abline(h = 0)
   }
@@ -30,74 +29,62 @@ TMMnorm <- function(exprData = RawTranscript,
                     annot = RawTranscript_annot,
                     Atrim = c(0, 8),
                     Mtrim = 0.02) {
-  #samples.names=as.data.frame.vector(colnames(exprData))
-  #genes.number=dim(exprData)
-  #genes.names=as.data.frame.vector(row.names(exprData))
-  #genes.level=as.data.frame.vector(rowSums(exprData))
-  
   # Scale data by their sequencing depth and multiply by 10^6 ##################
+  samples.name = colnames(exprData)
   samples.number = dim(exprData)[2]
-  samples.depth = as.data.frame.vector(
-    colSums(exprData)
-    )
-  exprData.scaled = exprData / t(samples.depth) * (10 ^ 6)
-  genes.length = annot$Length
+  samples.seqdepth = as.data.frame.vector(colSums(exprData, na.rm = TRUE))
+  exprData.scaled = exprData / t(samples.seqdepth) * (10 ^ 6)
   
   # Calculate SizeFactor #######################################################
+  genes.length = annot$Length
   samples.sizefactor = as.data.frame.vector(
     apply(exprData.scaled
-          ,MARGIN = 2
-          ,(function(x) return(sum(x * genes.length)))
-          )
+          , MARGIN = 2
+          , (function(x) return(sum(x * genes.length)))
     )
+  )
   
-  MvAplot(exprData.scaled, "mva.pdf")
+  MvAplot(exprData.scaled, "output/mva.pdf")
   
   # Calculate SF ScaleFactor ###################################################
   # as average of M
   # * by trimming the most extreme values of A
   # * by taking trimmed means of M values
-  SF.vector = rep(NaN, samples.number - 1) 
+  SF.vector = as.data.frame.vector(rep(NA,samples.number-1), length.out=samples.number - 1,row.names = colnames(exprData)[2:samples.number], col.names= "SF") # init vector
+  exprData.norm = exprData.scaled*0-1 # init exprData.norm 
   
   for (i in 2:samples.number) {
-    # M=log2(exprData.norm[,1]/samples.sizefactor[1,]*(samples.sizefactor[i,]/exprData.norm[,i]))
-    # A=0.5*(log2(exprData.norm[,1]/samples.sizefactor[1,])+log2(exprData.norm[,i]/samples.sizefactor[i,]))
-    ## remember -> exprData.scaled = exprData / t(samples.depth) * (10 ^ 6)
-    ## remember -> sample.depth = colSums(exprData)
-    M = log2(exprData.scaled[, 1] / exprData.scaled[, i])
-    A = 0.5 * (log2(exprData.scaled[, 1] * exprData.scaled[, i]))
-    temp = sort(M)
-    M.boundaries = c(temp[ceiling(1 + Mtrim * length(M))],
-                     temp[floor(length(M) * (1 - Mtrim))])
-    M.trimmed = M[(A > Atrim[1] & A < Atrim[2])
-                  & 
-                  (M > M.boundaries[1] & M < M.boundaries[2])
-                ]
-    SF.vector[i - 1] = mean(M.trimmed)
-    
-    ## trying to normalize data
-    exprData.norm[, i] = exprData.scaled[, i] + 2 ^ SF
-    
+    if(i!=1){
+      ## remember -> exprData.scaled = exprData / t(samples.depth) * (10 ^ 6)
+      ## remember -> sample.depth = colSums(exprData)
+      M = log2(exprData.scaled[, 1]) - log2(exprData.scaled[, i])
+      A = 0.5 * (log2(exprData.scaled[, 1]) + log2(exprData.scaled[, i]))
+      
+      M.Atrimmed = M[(A > Atrim[1] & A < Atrim[2])] ## most extreme values of A
+      SF = mean(M.Atrimmed,trim=Mtrim) ## trim extreme values of M 
+      SF.vector[i - 1,] = SF
+      
+      rm(M.Atrimmed,M,A)
+      
+      ## trying to normalize data
+      exprData.norm[i] = exprData.scaled[, i] + 2 ^ SF
+    }else{
+      exprData.norm[1]=exprData.scaled[1]
+    }
   }
   
-  MvAplot(exprData.norm, "mvanorm.pdf")
+  MvAplot(exprData.norm, "output/mvanorm.pdf")
   
   
   ### part 2
   # scale the genes (in original scale) by their length and multiply by 10^3
-  exprData.scaled.lenghtgene = exprData / genes.length * (10 ^ 3)
+  exprData.scaled = exprData / genes.length * (10 ^ 3)
   
   
-  # return(
-  #  matrix(c(SF.vector, exprData.scaled.lenghtgene))
-  # )
+  return(
+   c(SF.vector, exprData.scaled)
+  )
 }
-
-
-
-
-
-
 
 
 # TESTS
@@ -106,7 +93,12 @@ exprData = read.table("raw_trascr_count.txt")
 annot = read.delim("raw_trascr_count_annot.txt",
                    header = TRUE ,
                    sep = "\t")
-MvAplot(exprData, "MvAPlots.pdf")
+MvAplot(exprData, "output/MvAPlots.pdf")
 output = TMMnorm(exprData, annot)
-osf = output[1, ]
-odt = output[2, ]
+osf = output[1,]
+odt = output[2,]
+
+
+i=2
+Atrim = c(0, 8)
+Mtrim = 0.02 
