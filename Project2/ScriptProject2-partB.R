11cf5867a6d5f81b707cadc493293f12e1eac010
Andriolo Matteo <matteo.andriolo.2@studenti.unipd.it>
2022-12-06 12:42:03 +0100

    completed partB

--- Project2/ScriptProject2-partB.R
+++ Project2/ScriptProject2-partB.R
@@ -1,77 +1,89 @@
 library(edgeR)
 
-DATA<-read.table("raw_trascr_count.txt",sep="\t",row.names=1,header=TRUE)
-indGroup<-c(grep("Group1",colnames(DATA)),grep("Group2",colnames(DATA)))
-indCTRL<-grep("CTRL",colnames(DATA))
-lg<-length(indGroup)
-lc<-length(indCTRL)
+DATA <-
+  read.table(
+    "raw_trascr_count.txt",
+    sep = "\t",
+    row.names = 1,
+    header = TRUE
+  )
+indGroup <-
+  c(grep("Group1", colnames(DATA)), grep("Group2", colnames(DATA)))
+indCTRL <- grep("CTRL", colnames(DATA))
+lg <- length(indGroup)
+lc <- length(indCTRL)
 
-target=as.data.frame(rep("Patients", dim(DATA)[2]), row.names=colnames(DATA))
-colnames(target)=c("Groups")
-target[indCTRL,]="CTRL"
-group=factor(target$Groups)
-design=model.matrix(~0+group)
-
-#design = matrix(0, nrow=dim(DATA)[2], ncol = 2)
-#colnames(design)=c("Group","CTRL")
-#rownames(design)=colnames(DATA)
-#design[indGroup1,1]=1
-#design[indGroup2,1]=1
-#design[indCTRL,2]=1
-
-#target=matrix("",nrow=nrow(design), ncol=2)
-#for(i in 1:nrow(design)){
-#  if(design[i,1]==1){
-#    o="Group"
-#  }else{
-#    o="CTRL"
-#  }
-#  target[i,1]=rownames(design)[i]
-#  target[i,2]=o
-#}
+target = as.data.frame(rep("Patients", dim(DATA)[2]), row.names = colnames(DATA))
+colnames(target) = c("Groups")
+target[indCTRL, ] = "CTRL"
+group = factor(target$Groups)
+design = model.matrix( ~ 0 + group)
 
 
-#TODO write design file
-#fwrite(target,"design.txt",sep="\t", col.names=c("Sample","Group"), row.names=FALSE)
+y <- DGEList(counts = DATA)    # y is an object of type DGE
+y <-
+  calcNormFactors(y)   # This calculates the SF using the TMM normalization !!!
+SF <- y$samples
 
-
-#Group <- factor(as.data.frame.matrix(target)$V2)
-#target=model.matrix(~0+Group)
-#print(Group)
-
-
-y <- DGEList(counts=DATA)    # y is an object of type DGE
-y <- calcNormFactors(y)   # This calculates the SF using the TMM normalization !!!
-SF<-y$samples
-
-y <- estimateGLMCommonDisp(y,design, verbose=TRUE) #phi common to the entire dataset
-y <- estimateGLMTrendedDisp(y,design) #phi depends on mu
-y <- estimateGLMTagwiseDisp(y,design) #phi is gene specific
-fit <- glmFit(y,design) #finally the model fit (that accounts for raw NB data and scaling factors and seq. depth) 
+y <- 
+  estimateGLMCommonDisp(y, design, verbose = TRUE) #phi common to the entire dataset
+y <- estimateGLMTrendedDisp(y, design) #phi depends on mu
+y <- estimateGLMTagwiseDisp(y, design) #phi is gene specific
+fit <-
+  glmFit(y, design) #finally the model fit (that accounts for raw NB data and scaling factors and seq. depth)
 summary(fit)
 
-Confronti<-makeContrasts(Treatment ="groupCTRL-groupPatients",levels=design)
-RES<-glmLRT(fit,contrast=Confronti[,"Treatment"])
-# The first coloumn of RES reports the log_Fold_Change, i.e.: 
+Confronti <-
+  makeContrasts(Treatment = "groupCTRL-groupPatients", levels = design)
+RES <- glmLRT(fit, contrast = Confronti[, "Treatment"])
+# The first coloumn of RES reports the log_Fold_Change, i.e.:
 # log2(Normalized_data_average_GroupSARSCoV2 / Normalized_data_average_GroupMock)
 
-alpha=0.05
+alpha = 0.05
 
-selected=DATA[RES$table$PValue<alpha,]
-selected.Group=sum(selected[,indGroup])
-selected.CTRL=sum(selected[,indCTRL])
-notselected=DATA[!RES$table$PValue<alpha,]
-notselected.Group=sum(notselected[,indGroup])
-notselected.CTRL=sum(notselected[,indCTRL])
+selected = sum(RES$table$PValue < alpha)
+G=dim(DATA)[1]
+G0=0.8*G
+
+EFP=min(selected, G0*alpha)
+ETP=max(0,selected-EFP)
+ETN=min(G-selected, G0-EFP)
+EFN=G-EFP-ETP-ETN
+FDR=EFP/selected
+
+tablehptest=as.data.frame(matrix(c(ETN,EFP,G0,EFN,ETP,G-G0,G-selected,selected,G),nrow=3,byrow = TRUE),row.names = c("H0","H1"," "))
+colnames(tablehptest)=c("H0","H1"," ")
 
 
-salpha=selected.Group+selected.CTRL
-EFP=min(sum(DATA)*0.8*alpha, salpha)
-EFN=sum(DATA)-EFP-max(0,salpha-EFP)-min(sum(DATA)-salpha, sum(DATA)*0.8-EFP)
-FDR=EFP/salpha
+salpha = selected.Group + selected.CTRL
+EFP = min(sum(DATA) * 0.8 * alpha, salpha)
+EFN = sum(DATA) - EFP - max(0, salpha - EFP) - min(sum(DATA) - salpha, sum(DATA) *
+                                                     0.8 - EFP)
+FDR = EFP / salpha
+
+
+pvalues.vector=RES$table$PValue
+pvalues.rank=order(pvalues.vector)
+qvalues.benjhock=pvalues.vector*G/pvalues.rank
+
+out=list(
+  as.vector(
+    c(selected,
+      EFP,
+      EFN,
+      FDR),
+    mode = "any"
+  ),
+  matrix(c(pvalues.vector,qvalues.benjhock),
+         ncol = 2,
+         byrow=FALSE,
+         dimnames=list(row.names(RES$table),
+                       c("pvalues","qvalues.BenjHock"))
+  )
+)
 ##################################################################################
 
-DEbyEdgeR <- function(rawdat, groups, alpha=0.05){
+DEbyEdgeR <- function(rawdat, groups, alpha = 0.05) {
   return(c(selected.genes.alpha, EFP))
 }
 
@@ -79,8 +91,8 @@ DEbyEdgeR <- function(rawdat, groups, alpha=0.05){
 
 
 
-alpha=0.05
+alpha = 0.05
 rawdat = read.table("raw_trascr_count.txt")
-groups= colnames(rawdat)
-  
-DEbyEdgeR(rawdat,groups)
+groups = colnames(rawdat)
+
+DEbyEdgeR(rawdat, groups)
